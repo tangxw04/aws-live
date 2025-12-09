27558ee3e0dcf52b2a8142d04a25b7df1536c611
tangxw04 <tangxw-wp22@student.tarc.edu.my>
2025-12-09 15:26:48 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cnsg0yew24w8.us-east-1.rds.amazonaws.com"
+customuser = "aws_tangxiaowen"
+custompass = "Aa26264644."
+customdb = "employee"
+custombucket = "tangxiaowen-employee"
+customregion = "us-east-1"
+
